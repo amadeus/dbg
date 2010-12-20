95e7c03241ddc5e02bb11eb3bdc473bc775c8fb3
amadeus <amadeusdemarzi@gmail.com>
2010-12-20 12:25:47 -0800

    * subtle code tweaks to improve whitespace. * Comments are really unnecessary

--- Source/dbg.js
+++ Source/dbg.js
@@ -1,29 +1,23 @@
 (function(){
-	// Get real console, if it exists
-	var realConsole = window.console || null;
+	var realConsole = window.console || null,
+		fn = function(){},
+		disabledConsole = {
+			log: fn,
+			warn: fn,
+			info: fn,
+			enable: function(quiet){
+				window.dbg = realConsole ? realConsole : disabledConsole;
+				if (!quiet) window.dbg.log('dbg enabled.');
+			},
+			disable:function(){
+				window.dbg = disabledConsole;
+			}
+		};
 
-	var fn = function(){};
-
-	// Create disabled console
-	var disabledConsole = {
-		log: fn,
-		warn: fn,
-		info: fn,
-		enable: function(quiet){
-			window.dbg = realConsole ? realConsole : disabledConsole;
-			if (!quiet) window.dbg.log('dbg enabled.');
-		},
-		disable:function(){
-			window.dbg = disabledConsole;
-		}
-	};
-
-	// Extend real console with enable/disable
 	if (realConsole) {
 		realConsole.disable = disabledConsole.disable;
 		realConsole.enable = disabledConsole.enable;
 	}
 
-	// Setup dbg
 	disabledConsole.enable(true);
 })();
